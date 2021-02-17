2a69023a022b3ec72be595663bcb1ab0b335b096
llcain-github <llcain2012@gmail.com>
2021-02-16 19:37:25 -0600

    working navSlide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,38 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular-share-humanity';
 }
+
+
+
+// const navSlide = () => {
+//   const burger = document.querySelector('.burger');
+//   const nav = document.querySelector('.nav-links');
+//   const navLinks = document.querySelectorAll('.nav-links li');
+
+//   // Toggle Nav
+
+//   burger.addEventListener('click', () -> {
+
+//     // Toggle Nav
+
+//     nav.classList.toggle('nav-active');
+
+//     // Animation Links
+
+//     navLinks.forEach((link, index) => {
+//       if (link.style.animation) {
+//         link.style.animation = ''
+//       } else {
+//         link.style.animation = `navLink 0.5s ease forward ${index / 7 + 1.5}s`;
+//       }
+//     });
+
+//     // Burger Animation
+
+//     burger.classList.Toggle('toggle');
+
+//   });
+  
+// }
+
+// navSlide();
